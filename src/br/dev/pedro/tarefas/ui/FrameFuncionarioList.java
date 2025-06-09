e6cd6de190072e08d62567117132f102bc92a281
PEDRO HENRIQUE OLIVEIRA DA SILVA <25132766@CFP127ALUNO.LOCAL>
2025-06-09 15:08:57 -0300

    Criando Tela de Lista de Funcionarios

--- src/br/dev/pedro/tarefas/ui/FrameFuncionarioList.java
+++ src/br/dev/pedro/tarefas/ui/FrameFuncionarioList.java
@@ -1,16 +1,31 @@
 package br.dev.pedro.tarefas.ui;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class FrameFuncionarioList {
+	private JLabel labelTitulo;
+	private JButton btnCadastro;
+	private JTable tabelaFuncionarios;
+	private JScrollPane scrollFuncionarios;
+	private DefaultTableModel modelFuncionarios;
+	
+	//criando vetor para identificar colunas
+	private String[] colunas = {"CODIGO", "NOME", "CARGO"};
 	
 	public FrameFuncionarioList() {
 		criarTela();
 	}
 	
 	private void criarTela() {
+		
+		//criando objeto de tela
 		JFrame tela = new JFrame();
-		tela.setTitle("Lista de Tarefas");
+		tela.setTitle("Lista de Funcionarios");
 		tela.setSize(500, 500);
 		tela.setResizable(false);
 		tela.setLayout(null);	
